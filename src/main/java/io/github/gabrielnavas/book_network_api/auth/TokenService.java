e4bddd14c043bfe7dda685d541ed3a6abafd7fa5
navas <gabrielnavas@email.com>
2024-06-20 15:15:59 -0300

    change dynamic token length

--- src/main/java/io/github/gabrielnavas/book_network_api/auth/TokenService.java
+++ src/main/java/io/github/gabrielnavas/book_network_api/auth/TokenService.java
@@ -15,15 +15,16 @@ import java.time.LocalDateTime;
 public class TokenService {
 
     private final TokenRepository tokenRepository;
-
+    
     @Value("${application.security.register.token.characters-token}")
     private String charactersTokenCharacters;
-
     @Value("${application.security.register.token.expiration-in-minutes}")
     private long expirationInMinutes;
+    @Value("${application.security.register.token.token-length}")
+    private long tokenLength;
 
     public Token generateTokenAndSave(User user) {
-        String generateToken = generateActivationToken(6);
+        String generateToken = generateActivationToken();
         Token token = Token.builder()
                 .token(generateToken)
                 .createdAt(LocalDateTime.now())
@@ -33,10 +34,10 @@ public class TokenService {
         return tokenRepository.save(token);
     }
 
-    private String generateActivationToken(int length) {
+    private String generateActivationToken() {
         StringBuilder codeBuilder = new StringBuilder();
         SecureRandom secureRandom = new SecureRandom();
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < tokenLength; i++) {
             int randomIndex = secureRandom.nextInt(charactersTokenCharacters.length());
             char character = charactersTokenCharacters.charAt(randomIndex);
             codeBuilder.append(character);
