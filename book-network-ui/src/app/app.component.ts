58e10d22586b31a8b3af0a560578a6d87983bf80
navas <gabrielnavas@email.com>
2024-06-25 16:05:37 -0300

    add http client module

--- book-network-ui/src/app/app.component.ts
+++ book-network-ui/src/app/app.component.ts
@@ -3,11 +3,12 @@ import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {faCoffee} from '@fortawesome/free-solid-svg-icons';
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FontAwesomeModule],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
