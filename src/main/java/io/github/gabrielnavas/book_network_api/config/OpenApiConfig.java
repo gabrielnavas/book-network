a3990ba3d975b70ae91b7caa63e3b7e779cefae9
navas <gabrielnavas@email.com>
2024-06-25 15:09:52 -0300

    open api config

---
+++ src/main/java/io/github/gabrielnavas/book_network_api/config/OpenApiConfig.java
@@ -0,0 +1,49 @@
+package io.github.gabrielnavas.book_network_api.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "navasoka",
+                        email = "gabrielnavas@protonmail.com",
+                        url = "https://gabrielnavas.github.io"
+                ),
+                description = "OpenApi documentation for Spring Boot Security",
+                title = "OpenApi specification - navasoka",
+                version = "1.0",
+                license = @License(
+                        name = "license name",
+                        url = "https://some-url.com"
+                ),
+                termsOfService = "Terms of Service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8088/api/v1"
+                ),
+        },
+        security = @SecurityRequirement(
+                name = "barerAuth"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+
+}
