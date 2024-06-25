8b6a370c80e63cc53a9c39ac5d6b5ad8724e3d06
navas <gabrielnavas@email.com>
2024-06-25 15:40:04 -0300

    add fontawesome 6

--- book-network-ui/src/app/app.component.ts
+++ book-network-ui/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {faCoffee} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   helloWorld = 'hello World';
+  faCoffee = faCoffee;
 }
