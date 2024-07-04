4c3348c8df39ddd418361d7eeaf308576b08075d
navas <gabrielnavas@email.com>
2024-07-04 16:07:30 -0300

    add length on book request

--- book-network-api/src/main/java/io/github/gabrielnavas/book_network_api/book/BookRequest.java
+++ book-network-api/src/main/java/io/github/gabrielnavas/book_network_api/book/BookRequest.java
@@ -2,20 +2,25 @@ package io.github.gabrielnavas.book_network_api.book;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record BookRequest(
         Integer id,
         @NotNull(message = "100")
         @NotEmpty(message = "100")
+        @Size(min = 2, max = 100)
         String title,
         @NotNull(message = "101")
         @NotEmpty(message = "101")
+        @Size(min = 2, max = 50)
         String authorName,
         @NotNull(message = "102")
         @NotEmpty(message = "102")
+        @Size(min = 1, max = 50)
         String isbn,
         @NotNull(message = "103")
         @NotEmpty(message = "103")
+        @Size(min = 10, max = 500)
         String synopsis,
         boolean shareable
 ) {
